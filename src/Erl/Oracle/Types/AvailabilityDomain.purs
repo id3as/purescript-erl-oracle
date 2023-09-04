8dce84a50a118f887cbc7b846ad32769ada437f3
James Ogden <james@id3as.co.uk>
2023-09-04 10:28:39 +0100

    Add comment about remappoing name & id on availability doman

--- src/Erl/Oracle/Types/AvailabilityDomain.purs
+++ src/Erl/Oracle/Types/AvailabilityDomain.purs
@@ -5,6 +5,8 @@ module Erl.Oracle.Types.AvailabilityDomain
 import Data.Maybe (Maybe)
 import Erl.Oracle.Types.Common (AvailabilityDomainId, CompartmentId)
 
+-- The API returns "name" as a human readable string and "id" as a resource string (eg ocid1.availabilitydomain.oc1..aaaaaaaaw2i3i...)
+-- but the rest of the APIs use the name as the ID, not the id because that makes sense
 type AvailabilityDomain =
   { compartmentId :: Maybe CompartmentId
   , id :: Maybe AvailabilityDomainId
